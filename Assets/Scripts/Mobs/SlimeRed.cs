57fa2f9de786e405887344c7d0c03c9a550c76fb
Artorias Chang <zzhdsb123@gmail.com>
2020-05-08 22:23:30 -0400

    Red Slime added

---
+++ Assets/Scripts/Mobs/SlimeRed.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SlimeRed : Monster
+{
+    private void Start()
+    {
+        health = 70;
+        attack = 15;
+        defence = 2;
+        gold = 2;
+        exp = 2;
+    }
+}
