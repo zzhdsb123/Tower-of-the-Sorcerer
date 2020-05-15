c291393c555cedba5347c0a067fbf604ab2dc3e6
Artorias Chang <zzhdsb123@gmail.com>
2020-05-15 16:06:23 -0400

    level 8 completed

---
+++ Assets/Scripts/Mobs/JuniorWitch.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class JuniorWitch : Monster
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        health = 250;
+        attack = 120;
+        defence = 70;
+        gold = 20;
+        exp = 17;
+        initialDamage = 100;
+    }
+}
