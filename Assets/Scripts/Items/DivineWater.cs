f9838bca1628ff6db396adc9ddea8ad13b4dd666
Artorias Chang <zzhdsb123@gmail.com>
2020-05-17 16:59:07 -0400

    level 14 completed

---
+++ Assets/Scripts/Items/DivineWater.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DivineWater : MonoBehaviour, Objects
+{
+    public void Trigger()
+    {
+        Player player = FindObjectOfType<Player>();
+        player.health += player.health;
+        player.UpdateHealth();
+        this.gameObject.SetActive(false);
+    }
+
+}
