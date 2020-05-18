4dcd568098630390b89d4057149679f4f368cfa8
Artorias Chang <zzhdsb123@gmail.com>
2020-05-18 16:19:59 -0400

    level 18 completed

---
+++ Assets/Scripts/Mobs/Princess.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Princess : Monster
+{
+    public Canvas canvas;
+    public GameObject stairs;
+    private bool talked = false;
+    public override void Trigger()
+    {
+        if (!talked)
+        {
+            canvas.gameObject.SetActive(true);
+            stairs.gameObject.SetActive(true);
+            FindObjectOfType<PlayerMove>().enabled = false;
+            talked = true;
+        }
+    }
+}
--- Assets/Scripts/Mobs/Thief.cs
+++ Assets/Scripts/Mobs/Thief.cs
@@ -4,6 +4,8 @@ public class Thief : Monster
     public Canvas canvas;
     public GameObject secondFloorDoor;
     private bool doorOpened = false;
+    public GameObject damagedFloor;
+    public Canvas floorCanvas;
     // Start is called before the first frame update
     public override void Trigger()
     {
@@ -13,6 +15,14 @@ public class Thief : Monster
             FindObjectOfType<PlayerMove>().enabled = false;
             secondFloorDoor.GetComponent<SecondFloorDoor>().OpenDoor();
             doorOpened = true;
+        }
+
+        else if (FindObjectOfType<KeyItems>().floorTool)
+        {
+            canvas.gameObject.SetActive(true);
+            FindObjectOfType<PlayerMove>().enabled = false;
+            damagedFloor.gameObject.SetActive(false);
+            this.gameObject.SetActive(false);
         }
         
     }
