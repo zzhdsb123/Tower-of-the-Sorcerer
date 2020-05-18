0398826176ae3741f7300799f5af70bfe8d6183f
Artorias Chang <zzhdsb123@gmail.com>
2020-05-18 16:40:32 -0400

    level 19 completed

---
+++ Assets/Scripts/Items/HolyShield.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolyShield : MonoBehaviour, Objects
+{
+    void Objects.Trigger()
+    {
+        Player player = FindObjectOfType<Player>();
+        player.defence += 190;
+        player.UpdateDefence();
+        this.gameObject.SetActive(false);
+    }
+
+
+}
---
+++ Assets/Scripts/Items/HolySword.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolySword : MonoBehaviour,  Objects
+{
+    public void Trigger()
+    {
+        Player player = FindObjectOfType<Player>();
+        player.attack += 150;
+        player.UpdateAttack();
+        this.gameObject.SetActive(false);
+    }
+
+}
