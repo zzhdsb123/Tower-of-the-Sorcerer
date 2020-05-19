646ad2e59a7b1ae134bf3065c57fd6369549cc1f
Artorias Chang <zzhdsb123@gmail.com>
2020-05-19 16:38:11 -0400

    level 22 completed

---
+++ Assets/Scripts/General/Level22.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Level22 : Level
+{
+    public GameObject leftNextLevel, rightNextLevel;
+    public Transform positionFromLeft, positionFromRight;
+    // Update is called once per frame
+}
---
+++ Assets/Scripts/Stairs/StairsLeft.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairsLeft : MonoBehaviour, Objects
+{
+    public GameObject nextLevel;
+
+    public void Trigger()
+    {
+        FindObjectOfType<LevelController>().Jump(nextLevel);
+    }
+}
