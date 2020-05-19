a8191f06b5ba8511828ea96b49c8bf9ff2f49c95
Artorias Chang <zzhdsb123@gmail.com>
2020-05-19 17:54:18 -0400

    level 23 left completed

---
+++ Assets/Scripts/Items/SealKey3.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SealKey3 : MonoBehaviour, Objects
+{
+    public Canvas canvas;
+    public void Trigger()
+    {
+        canvas.gameObject.SetActive(true);
+        FindObjectOfType<PlayerMove>().enabled = false;
+        this.gameObject.SetActive(false);
+        FindObjectOfType<KeyItems>().sealKey3 = true;
+    }
+
+}
