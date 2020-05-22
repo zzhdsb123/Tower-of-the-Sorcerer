0b47833472457cb5df7c6626dd6db46f8bdce9ee
Artorias Chang <zzhdsb123@gmail.com>
2020-05-22 17:19:38 -0400

    full game 1.0 completed

---
+++ Assets/Scripts/Mobs/Dragon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Dragon : Monster
+{
+    public GameObject background;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        health = 99999;
+        attack = 5000;
+        defence = 4000;
+    }
+
+    public override void Die()
+    {
+        base.Die();
+        background.GetComponent<Animator>().SetTrigger("End");
+        Invoke("GameEnds", 1f);
+    }
+
+    void GameEnds()
+    {
+        SceneManager.LoadScene("Credit");
+    }
+}
