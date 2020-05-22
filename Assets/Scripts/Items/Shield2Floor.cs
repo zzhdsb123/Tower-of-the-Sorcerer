a7349223d7bb597cb91f7eec5d24c3e447977aad
Artorias Chang <zzhdsb123@gmail.com>
2020-05-21 23:39:36 -0400

    2nd floor sword shield added

---
+++ Assets/Scripts/Items/Shield2Floor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield2Floor : MonoBehaviour, Objects
+{
+    public void Trigger()
+    {
+        Player player = FindObjectOfType<Player>();
+        player.defence += 50;
+        player.UpdateDefence();
+        this.gameObject.SetActive(false);
+        FindObjectOfType<DoorOpenAuio>().PickUpItem();
+    }
+
+}
---
+++ Assets/Scripts/Items/Sword2Floor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword2Floor : MonoBehaviour, Objects
+{
+    public void Trigger()
+    {
+        Player player = FindObjectOfType<Player>();
+        player.attack += 70;
+        player.UpdateAttack();
+        this.gameObject.SetActive(false);
+        FindObjectOfType<DoorOpenAuio>().PickUpItem();
+    }
+
+
+}
